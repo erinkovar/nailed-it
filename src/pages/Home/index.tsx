23ea966c852451839f36b4e3610bcac7ac133f55
Brandon Gonzales <brandon.gonzales@credera.com>
2022-06-17 15:54:23 -0500

    reset home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Devs from '../../lotties/developers.json';
 import './home.css';
+import Lottie from 'react-lottie';
 
 function HomePage() {
   const defaultOptions = {
@@ -11,9 +12,17 @@ function HomePage() {
 
   return (
     <div className="home-page">
+      <Lottie options={defaultOptions} height={500} width={500} />
       <div>
-        
-
+        <h1 className="home-title">NAILED IT!</h1>
+        <p className="home-subtitle">
+          Welcome to XD Creative Field Day | "NAILED IT"... A chance for
+          designers and developers to collaborate on something fun and creative
+          for the day. It’s like a casual hackathon but with a twist.
+        </p>
+        <p className="home-subtitle">
+          It’s like a casual hackathon but with a TWIST.
+        </p>
       </div>
     </div>
   );
