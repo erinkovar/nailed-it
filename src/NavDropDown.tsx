98b7ad99141b20fb2922b1bb54106744d40b2b02
Brandon Gonzales <brandon.gonzales@credera.com>
2022-06-17 15:29:17 -0500

    more fixes

--- src/NavDropDown.tsx
+++ src/NavDropDown.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function NavDropDown() {
     const [isActivated, setActivation ] = useState(false);
@@ -21,6 +22,10 @@ function NavDropDown() {
             <div style={isActivated ? activeStyle : hiddenStyle } id="plinkoContainer"> 
                 <button id="addPlinko">Add Plinko</button> 
                 <div id="plinko" ></div>
+                <Link className="nav" to="thanks">Thank You</Link>
+                <Link className="nav" to="objective">Objective</Link>
+                <Link className="nav" to="teams">Teams</Link>
+                <Link className="nav" to="/">Home</Link>
             </div>
         </>
     );
