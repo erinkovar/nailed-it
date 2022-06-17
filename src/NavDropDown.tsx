6e5141bd0b23203c35d2dd37681edfe090e885fe
Brandon Gonzales <brandon.gonzales@credera.com>
2022-06-17 13:04:33 -0500

    menu drop down

--- src/NavDropDown.tsx
+++ src/NavDropDown.tsx
@@ -3,13 +3,22 @@ import { useState } from "react";
 
 function NavDropDown() {
     const [isActivated, setActivation ] = useState(false);
+
+    const hiddenStyle = {
+        display: 'none'
+    };
+
+    const activeStyle = {
+        animation: '0.3s ease-out expand'
+    };
+
     
     return(
         <>
             <nav onClick={() => {setActivation(!isActivated)}} className="app-nav" >
             {isActivated ? "↑ Menu ↑" : "↓ Menu ↓"}
             </nav>
-            <div id="plinko"> Hey </div>
+            <div style={isActivated ? activeStyle : hiddenStyle } id="plinko"> Hey </div>
         </>
     );
 }
